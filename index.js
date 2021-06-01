f737c1d424247faeac26a8ea3eb45ebab0104c4a
Ramilko37 <galyamdin@gmail.com>
2021-06-01 13:38:57 +0300

    cartModal popup open/close, escape, overlay

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-const cartModal = document.querySelector('.cartmodal');
+const cartModal = document.querySelector('.popup');
 const openCartBtn = document.querySelector('.cart__button');
 
 console.log(openCartBtn)
 
 function openCartModal() {
-    cartModal.classList.toggle('.cartmodal_visible');
-    console.log('раз')
+    cartModal.classList.add('popup_opened');
+    document.addEventListener('keydown', closeModalEscape);
 }
 
+function closeCartModal() {
+    cartModal.classList.remove('popup_opened');
+    document.removeEventListener('keydown', closeModalEscape);
+}
+
+function closeCartModalOverlay(e) {
+    if (!e.target.closest('.cartmodal')){
+        closeCartModal(e.target.closest('.popup'));
+        console.log('122323')
+    }
+};
+
+function closeModalEscape(evt) {
+    if (evt.key === 'Escape') {
+        closeCartModal();
+        console.log('escape')
+    }
+};
+
+
+
+
 openCartBtn.addEventListener('click', openCartModal);
+cartModal.addEventListener('click', closeCartModalOverlay);
